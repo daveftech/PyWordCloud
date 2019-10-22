dc5e5396eef54baa5e6c3f2141cea5c01b49f57c
Dave Fardoe <daveftech@gmail.com>
2019-10-22 12:24:21 -0500

    Plots with speech data

--- cloudgenerator.py
+++ cloudgenerator.py
@@ -6,24 +6,27 @@ from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 
 import matplotlib.pyplot as plt
 
-# Read in the data file
-df = pd.read_csv('test_data/winemag-data-130k-v2.csv')
+# Read in the data files
+f = open("data/2019_speech_trudeau.txt")
+trudeau_text = f.read()
+f.close()
 
-# Group the dataframe by country
-by_country = df.groupby('country')
-
-# Combine the text for all the reviews for canada
-canada = " ".join(review for review in df[df["country"] == "Canada"].description)
+f = open("data/2019_speech_sheer.txt")
+sheer_text = f.read()
+f.close()
 
 # Load the mask
 mask = np.array(Image.open("masks/canada_mapleleaf_mask.png"))
-
-wordcloud = WordCloud(background_color = "white", mode = "RGBA", max_words = 1000, mask = mask).generate(canada)
-
 image_colors = ImageColorGenerator(mask)
-print(image_colors)
 
-plt.figure(figsize=[7,7])
-plt.imshow(wordcloud.recolor(color_func=image_colors), interpolation="bilinear")
-plt.axis("off")
+wc_trudeau = WordCloud(background_color = "white", mode = "RGBA", max_words = 1000, mask = mask).generate(trudeau_text)
+wc_sheer = WordCloud(background_color = "white", mode = "RGBA", max_words = 1000, mask = mask).generate(sheer_text)
+
+fig, axes = plt.subplots(1, 2)
+axes[0].imshow(wc_trudeau.recolor(color_func=image_colors), interpolation="bilinear")
+axes[0].axis("off")
+axes[0].set_title("Election Speech: Trudeau")
+axes[1].imshow(wc_sheer.recolor(color_func=image_colors), interpolation="bilinear")
+axes[1].axis("off")
+axes[1].set_title("Election Speech: Sheer")
 plt.show()
