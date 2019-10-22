2919d00742f8a436cc9e64c0343b21060a3f0d52
Dave Fardoe <daveftech@gmail.com>
2019-10-22 10:41:28 -0500

    Printing some summary statistics for the test data

--- cloudgenerator.py
+++ cloudgenerator.py
@@ -8,3 +8,17 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv('test_data/winemag-data-130k-v2.csv')
 print(df.head())
+
+print("There are {} observations and {} features in this dataset. \n".format(df.shape[0],df.shape[1]))
+
+print("There are {} types of wine in this dataset such as {}... \n".format(len(df.variety.unique()), ", ".join(df.variety.unique()[0:5])))
+
+print("There are {} countries producing wine in this dataset such as {}... \n".format(len(df.country.unique()), ", ".join(df.country.unique()[0:5])))
+
+print(df[['country', 'description', 'points']].head())
+
+by_country = df.groupby('country')
+
+print(by_country.describe().head())
+
+print(by_country.mean().sort_values(by = "points", ascending = False).head())
