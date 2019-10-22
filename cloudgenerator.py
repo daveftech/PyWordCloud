1136652cae00eae643a0411a145d3d72a4d04150
Dave Fardoe <daveftech@gmail.com>
2019-10-22 11:11:47 -0500

    Basic word cloud working

--- cloudgenerator.py
+++ cloudgenerator.py
@@ -22,3 +22,44 @@ by_country = df.groupby('country')
 print(by_country.describe().head())
 
 print(by_country.mean().sort_values(by = "points", ascending = False).head())
+
+# Generate some plots from the data, to explore it a bit
+
+# plt.figure(figsize=(15, 10))
+# by_country.size().sort_values(ascending = False).plot.bar()
+# plt.xticks(rotation = 50)
+# plt.xlabel("Country of Origin")
+# plt.ylabel("Number of Wines")
+# plt.show()
+
+# plt.figure(figsize=(15,10))
+# by_country.max().sort_values(by="points",ascending=False)["points"].plot.bar()
+# plt.xticks(rotation=50)
+# plt.xlabel("Country of Origin")
+# plt.ylabel("Highest point of Wines")
+# plt.show()
+
+# Generate a basic word cloud
+# text = df.description[0]
+# wordcloud = WordCloud().generate(text)
+
+# Try our real text
+f = open("data/2109_speech_trudeau.txt")
+text = f.read()
+f.close()
+
+wordcloud = WordCloud(background_color = 'white').generate(text)
+
+plt.imshow(wordcloud, interpolation = 'bilinear')
+plt.axis("off")
+plt.show()
+
+f = open("data/2019_speech_sheer.txt")
+text = f.read()
+f.close()
+
+wordcloud = WordCloud(background_color = 'white').generate(text)
+
+plt.imshow(wordcloud, interpolation = 'bilinear')
+plt.axis("off")
+plt.show()
