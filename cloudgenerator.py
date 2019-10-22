54f96a88ce77781fa4bf65465f14e7408b082991
Dave Fardoe <daveftech@gmail.com>
2019-10-22 11:30:58 -0500

    Create masked plot

--- cloudgenerator.py
+++ cloudgenerator.py
@@ -6,6 +6,13 @@ from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 
 import matplotlib.pyplot as plt
 
+def transform_format(val):
+    '''Utility function to support transforming 0 values to 255 in the mask files'''
+    if val == 0:
+        return 255
+    else:
+        return val
+
 df = pd.read_csv('test_data/winemag-data-130k-v2.csv')
 print(df.head())
 
@@ -43,23 +50,43 @@ print(by_country.mean().sort_values(by = "points", ascending = False).head())
 # text = df.description[0]
 # wordcloud = WordCloud().generate(text)
 
-# Try our real text
-f = open("data/2109_speech_trudeau.txt")
-text = f.read()
-f.close()
+# # Try our real text
+# f = open("data/2109_speech_trudeau.txt")
+# text = f.read()
+# f.close()
 
-wordcloud = WordCloud(background_color = 'white').generate(text)
+# wordcloud = WordCloud(background_color = 'white').generate(text)
 
-plt.imshow(wordcloud, interpolation = 'bilinear')
-plt.axis("off")
-plt.show()
+# plt.imshow(wordcloud, interpolation = 'bilinear')
+# plt.axis("off")
+# plt.show()
 
-f = open("data/2019_speech_sheer.txt")
-text = f.read()
-f.close()
+# f = open("data/2019_speech_sheer.txt")
+# text = f.read()
+# f.close()
 
-wordcloud = WordCloud(background_color = 'white').generate(text)
+# wordcloud = WordCloud(background_color = 'white').generate(text)
 
-plt.imshow(wordcloud, interpolation = 'bilinear')
+# plt.imshow(wordcloud, interpolation = 'bilinear')
+# plt.axis("off")
+# plt.show()
+
+# Generate a masked word cloud
+wine_mask = np.array(Image.open("masks/wine_mask.png"))
+print(wine_mask.shape)
+
+transformed_wine_mask = np.ndarray((wine_mask.shape[0], wine_mask.shape[1]), np.int32)
+
+for i in range(len(wine_mask)):
+    for j in range(len(wine_mask[i])):
+        transformed_wine_mask[i][j] = transform_format(wine_mask[i][j][0])
+
+print(transformed_wine_mask)
+
+wc = WordCloud(background_color='white', max_words=1000, mask=transformed_wine_mask, contour_width=3, contour_color='firebrick')
+text = df.description[0]
+wc.generate(text)
+plt.figure(figsize=[20,10])
+plt.imshow(wc, interpolation='bilinear')
 plt.axis("off")
 plt.show()
